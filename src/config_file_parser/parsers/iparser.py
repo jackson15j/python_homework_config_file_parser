b6a66d2f8dfa4dd465b8e992bc2a5ffe06ca8bd1
Craig Astill <craig.astill@gmail.com>
2023-01-30 11:42:36 +0000

    Added an `IParser` interface for future JSON Parser to inherit.
    * AbstractMethod for reading files and parsing file to `parsed_dict`.
    * Implement getters/setters for `parsed_dict` and getters for file lists.
    
    Issue: #6.

---
+++ src/config_file_parser/parsers/iparser.py
@@ -0,0 +1,47 @@
+"""Interface that all Parser's inherit."""
+
+from abc import ABC, abstractmethod
+
+
+class IParser(ABC):
+    """Base Parser."""
+
+    def __init__(self):
+        self._files: list = []
+        self._parsed_files: list = []
+        self._parsed_dict: dict = {}
+
+    @property
+    def files(self) -> list:
+        """Returns list of supplied files to parse."""
+        return self._files
+
+    @property
+    def parsed_files(self) -> list:
+        """Returns list of files that were parsed."""
+        return self._parsed_files
+
+    @property
+    def parsed_dict(self) -> dict:
+        """Returns Dict containing a parsed version from all of the config
+        files.
+        """
+        return self._parsed_dict
+
+    @parsed_dict.setter
+    def _set_parsed_dict(self, val: dict) -> None:
+        """Updates the parsed Dict with new values.
+
+        ..NOTE::
+        Expected to be used only in tests!
+        """
+        self._parsed_dict.update(val)
+
+    @abstractmethod
+    def parse(self, files: list) -> None:
+        """Parse files into the instances `parsed_dict`.
+
+        :param list files: List of files to parse. Note that subsequent files
+            **will** override the value of any prior duplicate keys!
+        :returns: None.
+        """
