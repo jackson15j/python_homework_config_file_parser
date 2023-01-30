4c5f9fc931b5d58ab9d6292c881397ad56981f8b
Craig Astill <craig.astill@gmail.com>
2023-01-30 13:55:45 +0000

    Update `IParser` unit test to new `parse()` implementation.
    Issue: #6.

--- tests/unit/parsers/test_iparser.py
+++ tests/unit/parsers/test_iparser.py
@@ -4,10 +4,29 @@ from src.config_file_parser.parsers.iparser import IParser
 
 
 class FakeParser(IParser):
-    """Test Parser class with enough to satisfy ABC."""
+    """Test Parser class with enough to satisfy ABC.
 
-    def parse(self, files: list):
-        return None
+    ..NOTE::
+
+        In non-interface code, where I was directly using the class under test,
+        I would typically test in the following ways:
+
+        * Monkey Patch if I need to inject state into a variable/call buried
+          within a function.
+        * Refactor buried variable/call to be passed into the function under
+          test. ie. harden contract boundaries, decouple code, remove the need
+          to Monkey Patch.
+    """
+
+    _inject_parsed_dict: dict = {}
+
+    def _read_file_contents(self, _):
+        """Overriding this method since it will be done in an integration test
+        or downstream unittests with `tmp_path`."""
+        return ""
+
+    def _parse_content(self, _):
+        return self._inject_parsed_dict
 
 
 class TestIParser:
@@ -29,7 +48,7 @@ class TestIParser:
         exp = {"a": 1}
         parser = FakeParser()
         assert parser.parsed_dict == {}
-        parser._set_parsed_dict = exp
+        parser.parsed_dict = exp
         assert parser.parsed_dict == exp
 
     @pytest.mark.parametrize(
@@ -39,10 +58,23 @@ class TestIParser:
             ({"b": 2, "c": 2}, {"a": 1, "b": 1}, {"a": 1, "b": 1, "c": 2}),
         ),
     )
-    def test_parsed_dict_updates(self, dict_a, dict_b, exp):
+    def test_file_parsing_to_parsed_dict_updates(
+        self, dict_a, dict_b, exp, tmp_path
+    ):
+        """End-to-End test of `IParser.parse()`, but mocked out the file
+        handling to keep it as a unit test."""
         parser = FakeParser()
+        # NOT populating temp files with content, due to overriding the
+        # file-handling code.
+        file_a = tmp_path / "file_a"
+        file_b = tmp_path / "file_b"
+
         assert parser.parsed_dict == {}
-        parser._set_parsed_dict = dict_a
+        # Injecting state due to overriding file reads.
+        parser._inject_parsed_dict = dict_a
+        parser.parse([file_a])
         assert parser.parsed_dict == dict_a
-        parser._set_parsed_dict = dict_b
+        parser._inject_parsed_dict = dict_b
+        # Explicitly parse multiple _"files"_.
+        parser.parse([file_b])
         assert parser.parsed_dict == exp
