f46b2eacb0340f83e4a1388b7ed924fce8c16fbd
Craig Astill <craig.astill@gmail.com>
2023-01-31 21:33:47 +0000

    [SCOPE CREEP] Implemented ability to lookup keys with periods.
    This is definitely scope creep to actually knock up support for looking a key
    with a period in it!! Ideally, I should have just left it as a
    query/observation, but had the itch to highlight and _"just do it!"_ (Can
    either be appreciated or seen as a warning sign).
    
    Change allows the escaped Full Stop to pass through as part of the Key lookup,
    instead of splitting and having a false negative look up.
    
    Issue: #7.

--- src/config_file_parser/readers/reader.py
+++ src/config_file_parser/readers/reader.py
@@ -2,16 +2,31 @@
 from typing import Union
 
 
-def get(path: str, parsed_dict: dict) -> Union[None, dict, tuple, list, str, int]:
+def _to_str(item: Union[str, bytes]) -> str:
+    """Decode to str helper to get around `mypy` type noise in upstream
+    str-only functions."""
+    if isinstance(item, bytes):
+        # Handle RFC7159/JSON-spec which allows Full Stops to be used in the key
+        return item.decode("utf-8")
+    return item
+
+
+def get(
+    path: Union[str, bytes], parsed_dict: dict
+) -> Union[None, dict, tuple, list, str, int]:
     """Getter that is used to look up the single/section value(s) in the parsed
     config from a supplied dot path.
 
-    :param str path: Dot notation path into the parsed config. eg.
-        "key1.subkey7".
+    :param str|bytes path: Dot notation path into the parsed config.
+        eg. "key1.subkey7".
+        NOTE: `path` must be a bytes object to allow passing `\u002e` to do
+        look-ups of a key containing a full stop!
+        eg. Key = "a.b", send: `b"a\u002eb"`.
     :param dict parsed_dict: Dict to do a lookup against.
     :returns: The value of the key, whether it is a single value or a section.
     """
-    _keys = path.split(".")
+    _path: str = _to_str(path)
+    _keys = _path.split(".")
     cmd_str = "parsed_dict"
     for _key in _keys:
         cmd_str += f".get('{_key}', {{}})"
