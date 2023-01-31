3aa89ca8b1f01a50a5427b47eb64036a2a871fd2
Craig Astill <craig.astill@gmail.com>
2023-01-31 21:29:48 +0000

    Unit Test requirement query on looking up periods in JSON key.
    The JSON spec (RFC7159) allows any non-special escaped character in a string,
    so full stops in a key is legal (eg. `{"a.b": 1}`), but the current
    requirements leave this as an unstated requirement/issue.
    
    Issues: #3,#7.

--- tests/unit/readers/test_reader.py
+++ tests/unit/readers/test_reader.py
@@ -25,3 +25,23 @@ class TestDottedPathReaderInitialRequirements:
 
     def test_get_returns_section_value(self):
         assert reader.get("cache", PARSED_DICT) == PARSED_DICT["cache"]
+
+    def test_get_supports_escaped_period_for_keys_with_a_period(self):
+        """Test to: RAISE AS A CHALLENGE TO BASE REQUIREMENTS.
+
+        The JSON Spec supports any unicode character (apart from an unrelated
+        subset) in a string. Therefore, it is legal to have a period in a
+        key. Current requirements of the problem don't state this, but this
+        would most like be:
+
+        - Added as a future requirement.
+        - Raised as a Customer bug.
+        - Documented as a design/known issue.
+
+        On the assumption that it would be requested, Adding a quick test to
+        support the escaped version for a Full Stop (`U+002E`).
+        """
+        exp = 1
+        _parsed_dict = {"a.b": exp}
+        assert reader.get("a.b", _parsed_dict) == {}
+        assert reader.get(r"a\u002eb", _parsed_dict) == exp
