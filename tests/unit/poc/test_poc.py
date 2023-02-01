9949524c4067d7721c305c1970d7672858fe7e7c
Craig Astill <craig.astill@gmail.com>
2023-02-01 21:50:41 +0000

    Bulk up PoC unit tests.
    - Split single file tests into:
      - Invalid JSON parsing tests (if the future changes to bubbling exception
        up).
      - Valid JSON, with dotted-path at different levels.
    - Unit test for all supplied files+content.
    
    Issue: #3.

--- tests/unit/poc/test_poc.py
+++ tests/unit/poc/test_poc.py
@@ -12,6 +12,27 @@ class TestPoC:
         (
             ("", "not.found", {}),
             ({}, "not.found", {}),
+        ),
+    )
+    def test_graceful_response_from_invalid_parsed_content(
+        self, data, dot_path, exp, tmp_path
+    ):
+        """Verify invalid file contents parses to an arbitrary empty value,
+        instead of throwing an exception.
+
+        ..NOTE::
+          Separated from below identical test, to support a Production code
+          change to test Parser Exceptions being reported back to the User.
+        """
+        f = tmp_path / "file.json"
+        f.write_text(json.dumps(data))
+
+        parser = PoC([f])
+        assert parser.look_up(dot_path) == exp
+
+    @pytest.mark.parametrize(
+        "data,dot_path,exp",
+        (
             ({"a": 1}, "a", 1),
             ({"a": [1, 2]}, "a", [1, 2]),
             ({"a": {"b": 2}}, "a", {"b": 2}),
@@ -19,6 +40,7 @@ class TestPoC:
         ),
     )
     def test_parse_single_file(self, data, dot_path, exp, tmp_path):
+        """Verify dotted look-ups return the expected singular/section value."""
         f = tmp_path / "file.json"
         f.write_text(json.dumps(data))
 
@@ -45,6 +67,9 @@ class TestPoC:
         ),
     )
     def test_parse_multiple_files(self, data1, data2, dot_path, exp, tmp_path):
+        """Verify that when we consolidate files, it overrides later files in
+        the sequence over earlier.
+        """
         f1 = tmp_path / "file1.json"
         f1.write_text(json.dumps(data1))
         f2 = tmp_path / "file2.json"
@@ -52,3 +77,58 @@ class TestPoC:
 
         parser = PoC([f1, f2])
         assert parser.look_up(dot_path) == exp
+
+    def test_with_supplied_example_files(self, tmp_path):
+        """Unit test with the filenames and contents of the fixture files
+        supplied to verify the functionality of this Code Test.
+        """
+        f1 = tmp_path / "config.json"
+        f1.write_text(
+            json.dumps(
+                {
+                    "environment": "production",
+                    "database": {
+                        "host": "mysql",
+                        "port": 3306,
+                        "username": "divido",
+                        "password": "divido",
+                    },
+                    "cache": {"redis": {"host": "redis", "port": 6379}},
+                }
+            )
+        )
+        f2 = tmp_path / "config.also_invalid.json"
+        f2.write_text("This is not a valid JSON file either")
+        f3 = tmp_path / "config.invalid.json"
+        f3.write_text("This is not a valid JSON file")
+        f4 = tmp_path / "config.local.json"
+        f4.write_text(
+            json.dumps(
+                {
+                    "environment": "development",
+                    "database": {
+                        "host": "127.0.0.1",
+                        "port": 3306,
+                        "username": "divido",
+                        "password": "divido",
+                    },
+                    "cache": {"redis": {"host": "127.0.0.1", "port": 6379}},
+                }
+            )
+        )
+
+        # Alphabetical file name order.
+        parser = PoC([f1, f2, f3, f4])
+        assert parser.look_up("environment") == "development"
+        assert parser.look_up("database.host") == "127.0.0.1"
+        assert parser.look_up("cache") == {
+            "redis": {"host": "127.0.0.1", "port": 6379}
+        }
+
+        # Reverse alphabetical file name order.
+        parser2 = PoC([f4, f3, f2, f1])
+        assert parser2.look_up("environment") == "production"
+        assert parser2.look_up("database.host") == "mysql"
+        assert parser2.look_up("cache") == {
+            "redis": {"host": "redis", "port": 6379}
+        }
