6f3a8cd20ded074049ac6008db6b4ab038bd235d
Craig Astill <craig.astill@gmail.com>
2023-02-02 01:15:36 +0000

    Add CLI argparsing to have an entrypoint for integration testing.
    Also can be used in Docker.
    
    Issue: #3.

--- src/config_file_parser/poc/poc.py
+++ src/config_file_parser/poc/poc.py
@@ -1,6 +1,7 @@
 """Proof of Concept to quickly parse a set of JSON files gracefully and allow a
 dotted-path lookup of the consolidated structure."""
 import json
+from argparse import ArgumentParser
 from pathlib import Path
 from typing import Any
 
@@ -58,3 +59,46 @@ class PoC:
         # un-validated, malformed strings passed in by the User.
         ret_val = eval(cmd_str)
         return ret_val
+
+
+def get_cli_args() -> ArgumentParser:
+    """Return a CLI args parser instance for downstream EntryPoint code to call
+    `parse_args() on.
+    """
+    cli_parser = ArgumentParser(
+        prog="ConfigFileParser",
+        description=(
+            "Read/Parse/Consolidate multiple JSON files and then allow "
+            "looking up values via a dotted notation."
+        ),
+    )
+    cli_parser.add_argument(
+        "dotted_path",
+        type=str,
+        help=(
+            "Dotted notation to do look-ups in the files provided. eg. "
+            "`KeyX.SubKeyY.SubSubKeyZ`."
+        ),
+    )
+    cli_parser.add_argument(
+        "files",
+        nargs="+",
+        type=Path,
+        help=(
+            "Space separated list of 1+ files to Consolidate together (in listed "
+            "order) to do a look-up from. NOTE: Latter files will override "
+            "earlier files during consolidation. NOTE: Consolidation is handled "
+            "in memory - Files are only read by the Application!"
+        ),
+    )
+    return cli_parser
+
+
+def get() -> Any:
+    """Function to call the PoC class + lookup from a Python package
+    entry-point. See: https://peps.python.org/pep-0621/#entry-points.
+    """
+    args = get_cli_args().parse_args()
+
+    parser = PoC(args.files)
+    return parser.look_up(args.dotted_path)
